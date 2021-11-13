a42fd25a8185bf9836151a7aa1146deb8d0445a6
harverma <85237897+harverma@users.noreply.github.com>
2021-11-14 01:59:49 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,1 +1,5 @@
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SQL Server 2019\Enterprise Edition'  'C:\MSSQL2019\'
+C:\MSSQL2019\SW_DVD9_SQL_Svr_Enterprise_Edtn_2019Dec2019_64Bit_English_MLF_X22-22247.ISO
+$configfile = "C:\MSSQL2019\configuration.ini"
+$command = "D:\setup.exe /ConfigurationFile=$($configfile)"
+Invoke-Expression -Command $command
