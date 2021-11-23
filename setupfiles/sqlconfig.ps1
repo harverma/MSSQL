9ad160e8c2ed59a67d0417738ebda4e975217fd5
harverma <85237897+harverma@users.noreply.github.com>
2021-11-23 21:05:43 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
-robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SQL Server 2019\Enterprise Edition'  'C:\MSSQL2017\'
+robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\MSSQL Automation\2017'  'C:\MSSQL2017\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
-C:\MSSQL2017\SW_DVD9_NTRL_SQL_Svr_Standard_Edtn_2017_64Bit_English_OEM_VL_X21-56945.ISO
+C:\MSSQL2017\SW_DVD9_SQL_Svr_Enterprise_Edtn_2017_64Bit_English_MLF_X21-56962.ISO
 sllep -s 5
