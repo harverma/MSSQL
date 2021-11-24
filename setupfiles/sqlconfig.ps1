a60076bda200062041e81bfd2a17804b3b7b1443
harverma <85237897+harverma@users.noreply.github.com>
2021-11-24 19:13:18 +0530

    sqlpath

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
-robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\MSSQL Automation\2016'  'C:\MSSQL2016\'
+robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2016'  'C:\MSSQL2016\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
 C:\MSSQL2016\en_sql_server_2016_enterprise_with_service_pack_2_x64_dvd_12124051.iso
 sllep -s 5
