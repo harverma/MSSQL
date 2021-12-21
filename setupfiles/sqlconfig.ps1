af5084182cf203f902b3c5a2b5cbbf9dc4c1fd0a
harverma <85237897+harverma@users.noreply.github.com>
2021-12-22 03:05:37 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
-robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2019'  'C:\MSSQL2019\'
+robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2014'  'C:\MSSQL2014\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
-C:\MSSQL2019\SW_DVD9_SQL_Svr_Enterprise_Edtn_2019Dec2019_64Bit_English_MLF_X22-22247.ISO
+C:\MSSQL2014\SW_DVD9_SQL_Svr_Enterprise_Edtn_2014w_SP3_64Bit_English_MVLS_MLF_X21-70106.ISO
 sllep -s 5
