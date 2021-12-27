0876c8aeee76835be8bccc443c0630ea398ca26e
harverma <85237897+harverma@users.noreply.github.com>
2021-12-28 00:47:32 +0530

    Update sqlconfig.ps1

--- setupfiles/sqlconfig.ps1
+++ setupfiles/sqlconfig.ps1
@@ -1,4 +1,4 @@
-robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2017'  'C:\MSSQL2017\'
+robocopy  '\\172.20.32.12\Software\ansibleStaging\MSSQL Automation\2012'  'C:\MSSQL2012\'
 robocopy  '\\172.20.32.12\Software\Software\Microsoft Software\SSMS' 'C:\SSMS\'
-C:\MSSQL2017\SW_DVD9_SQL_Svr_Enterprise_Edtn_2017_64Bit_English_MLF_X21-56962.ISO
+C:\MSSQL2012\en_sql_server_2012_enterprise_edition_with_service_pack_4_x64_dvd_11444956.iso
 sllep -s 5
